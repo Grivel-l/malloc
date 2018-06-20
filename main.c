dd6c6ffb9092f308220f6a2c320273de34e1080a
Leo Grivel <legrivel@student.le-101.fr>
2018-06-20 05:46:30 +0200

    Malloc working

--- main.c
+++ main.c
@@ -1,20 +1,50 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/06/18 15:44:29 by legrivel     #+#   ##    ##    #+#       */
+/*   Updated: 2018/06/20 05:42:19 by legrivel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 #include "malloc.h"
 
 int		main(void)
 {
-	char	*yo;
+	char	*str;
+	/*str = malloc(1);
+	str = str - 1 - sizeof(t_chunk);
+	((t_chunk *)str)->next = ((t_chunk *)str) + sizeof(t_chunk) + 1;
+	((t_chunk *)str)->next->next = ((t_chunk *)str)->next + sizeof(t_chunk) + 1;
+	printf("HelloWorld\n");
+	return (0);*/
+	str = malloc(120);
+	printf("%p, %p, %p, %p\n", str, str + 120 + 24, str + 48 + 220, str + 72 + 320);
+	memset(str, 'A', 99);
+	str[99] = '\0';
+	printf("Str: %p, %s\n\n", str, str);
 
-	if ((yo = malloc(3)) == NULL)
-	{
-		printf("Error: %s\n", strerror(errno));
-		return (1);
-	}
-	yo[0] = 'y';
-	yo[1] = 'o';
-	yo[2] = '\n';
-	write(1, yo, 3);
-	printf("Hey: |%s|\n", yo);
+	str = malloc(100);
+	memset(str, 'B', 99);
+	str[99] = '\0';
+	printf("Str: %p, %s\n\n", str, str);
+
+	str = malloc(100);
+	memset(str, 'C', 99);
+	str[99] = '\0';
+	printf("Str: %p, %s\n\n", str, str);
+
+	str = malloc(100);
+	memset(str, 'D', 99);
+	str[99] = '\0';
+	printf("Str: %p, %s\n\n", str, str);
+
 	return (0);
 }
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,12 +6,13 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/17 23:31:46 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/06/18 15:48:18 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/06/20 05:31:14 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "malloc.h"
+#include <sys/resource.h>
 
 static t_chunk_types	g_chunks = {NULL, NULL, NULL};
 
@@ -21,5 +22,7 @@ void					*malloc(size_t size)
 
 	if ((chunk = init_chunks(&g_chunks, size)) == NULL)
 		return (NULL);
+	// printf("First pointer: %p, %p\n", g_chunks.tiny, chunk);
+// 	printf("All pointers: %p, %p, %p, %p\n", g_chunks.tiny + sizeof(t_chunk), g_chunks.tiny + sizeof(t_chunk) * 2 + 100, g_chunks.tiny + sizeof(t_chunk) * 3 + 200, g_chunks.tiny + sizeof(t_chunk) * 4 + 300);
 	return (chunk + sizeof(t_chunk));
 }
