1b26562790f0dfb4cd116b682e2866e7087d3140
Leo Grivel <legrivel@z2r2p4.le-101.fr>
2019-12-13 00:50:28 +0100

    Architecture

---
+++ srcs/free.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   free.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/13 00:36:51 by legrivel     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/13 00:36:53 by legrivel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+static void    free_in_chunk(t_chunk *chunk, t_chunk **chunks, int type)
+{
+    t_chunk   *tmp;
+    size_t    total;
+    size_t    page_size;
+    t_chunk   *previous;
+    size_t    page_freed;
+
+    total = 0;
+    tmp = *chunks;
+    page_freed = 1;
+    previous = NULL;
+    page_size = getpagesize();
+    while (tmp != chunk)
+    {
+      if (!tmp->freed)
+        page_freed = 0;
+      total += tmp->size + sizeof(t_chunk);
+      if (total > page_size * type)
+      {
+        previous = ((void *)tmp) - total;
+        total = tmp->size + sizeof(t_chunk);
+      }
+      tmp = tmp->next;
+    }
+    tmp->freed = 1;
+    while (tmp->next != NULL && total + tmp->size + sizeof(t_chunk) < page_size * type)
+    {
+      if (!tmp->freed)
+        page_freed = 0;
+      tmp = tmp->next;
+      total += tmp->size + sizeof(t_chunk);
+    }
+    if (!tmp->freed)
+      page_freed = 0;
+    if (page_freed)
+    {
+      if (previous == NULL)
+        *chunks = tmp->next;
+      else
+        previous->next = tmp->next;
+      tmp = ((void *)tmp) - total;
+      munmap(tmp, get_chunk_size(tmp->size, page_size * type));
+    }
+}
+
+static void    free_chunk(t_chunk *chunk, t_chunk **chunks, int type)
+{
+    t_chunk *tmp;
+    t_chunk *previous;
+
+    tmp = *chunks;
+    if (type == -1)
+    {
+      previous = NULL;
+      while (tmp != chunk)
+      {
+        previous = tmp;
+        tmp = tmp->next;
+      }
+      if (previous == NULL)
+        *chunks = tmp->next;
+      else
+        previous->next = chunk->next;
+      munmap(chunk, chunk->size);
+    }
+    else
+      free_in_chunk(chunk, chunks, type);
+}
+
+void    free(void *ptr)
+{
+    int     type;
+    t_chunk *chunk;
+
+    chunk = g_chunks[0];
+    if (ptr == NULL)
+      return ;
+    chunk = ptr - sizeof(t_chunk);
+    chunk->freed = 1;
+    type = get_type(chunk);
+    if (type == TINY)
+      free_chunk(chunk, &(g_chunks[0]), TINY);
+    else if (type == SMALL)
+      free_chunk(chunk, &(g_chunks[1]), SMALL);
+    else
+      free_chunk(chunk, &(g_chunks[2]), -1);
+}
---
+++ srcs/malloc.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   malloc.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/13 00:44:46 by legrivel     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/13 00:44:47 by legrivel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+static t_chunk   *create_chunk(t_chunk **chunk, size_t size, int chunk_size) {
+    if ((*chunk = mmap(NULL, chunk_size,
+  PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0)) == MAP_FAILED)
+      return (NULL);
+    (*chunk)->freed = 0;
+    (*chunk)->size = size;
+    (*chunk)->next = NULL;
+    return (*chunk);
+}
+
+static t_chunk   *append_chunk(t_chunk *chunk, size_t size)
+{
+    t_chunk new_chunk;
+
+    new_chunk.freed = 0;
+    new_chunk.size = size;
+    new_chunk.next = chunk->next;
+    chunk->next = ((void *)chunk) + chunk->size + sizeof(t_chunk);
+    ft_memcpy(chunk->next, &new_chunk, sizeof(t_chunk));
+    return (chunk->next);
+}
+
+static t_chunk   *get_next_chunk(t_chunk *chunk, size_t size, size_t chunk_size)
+{
+    size_t  total;
+    t_chunk *previous;
+    t_chunk *new_chunk;
+
+    total = 0;
+    previous = NULL;
+    while (chunk->next != NULL)
+    {
+      if (total + chunk->size + sizeof(t_chunk) > chunk_size &&
+        total + size + sizeof(t_chunk) <= chunk_size)
+        return (append_chunk(previous, size));
+      total += chunk->size + sizeof(t_chunk);
+      if (total > chunk_size)
+        total = chunk->size + sizeof(t_chunk);
+      previous = chunk;
+      chunk = chunk->next;
+    }
+    if (total + size + sizeof(t_chunk) * 2 + chunk->size <= chunk_size)
+      return (append_chunk(chunk, size));
+    if ((new_chunk = create_chunk((t_chunk **)(&(chunk->next)), size, chunk_size)) == NULL)
+      return (NULL);
+    return (new_chunk);
+}
+
+static t_chunk   *big_chunk(t_chunk **chunk, size_t size)
+{
+    t_chunk *tmp;
+
+    if (*chunk == NULL)
+      return (create_chunk(chunk, size, get_chunk_size(size, getpagesize())));
+    tmp = *chunk;
+    while (tmp->next != NULL)
+      tmp = tmp->next;
+    return (create_chunk(((t_chunk **)(&(tmp->next))), size, get_chunk_size(size, getpagesize())));
+}
+
+static t_chunk			*init_chunks(size_t size)
+{
+    size_t	max_tiny;
+    size_t      max_small;
+    int 	page_size;
+
+    page_size = getpagesize();
+    max_tiny = page_size * TINY / 100 - sizeof(t_chunk);
+    max_small = page_size * SMALL / 100 - sizeof(t_chunk);
+    if (size <= max_tiny && g_chunks[0] == NULL)
+      return (create_chunk(&(g_chunks[0]), size, page_size * TINY));
+    else if (size <= max_tiny && g_chunks[0] != NULL)
+      return (get_next_chunk(g_chunks[0], size, page_size * TINY));
+    else if (size <= max_small && g_chunks[1] == NULL)
+      return (create_chunk(&(g_chunks[1]), size, page_size * SMALL));
+    else if (size <= max_small && g_chunks[1] != NULL)
+      return (get_next_chunk(g_chunks[1], size, page_size * SMALL));
+    return (big_chunk(&(g_chunks[2]), size));
+}
+
+void    *malloc(size_t size)
+{
+    return (((void *)init_chunks(size)) + sizeof(t_chunk));
+}
